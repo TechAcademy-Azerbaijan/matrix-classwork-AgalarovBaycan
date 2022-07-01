474fc7b076c9f89c1f4da70636442aaa9a4f973e
AgalarovBaycan <106188929+AgalarovBaycan@users.noreply.github.com>
2022-07-01 20:56:58 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,4 +1,17 @@
-// Use this array for solution
-let array = [3, 5, -7, 7, 5, -9, -4]
+/* let arr = [3, 5, -7, 7, 5, -9, -4]
+let arr2 =[]
+let count = 0
 
-// Write code here. 
+for (let i = 0; i < arr.length; i++) {
+    for(let i = 0; i <arr.length; i++){
+        if(!arr2.includes(arr[i])){
+            arr2.push(arr[i])
+        }
+            
+    }
+   
+   
+
+
+}console.log(arr2.length);
+ */
